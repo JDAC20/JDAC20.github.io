452367077ac965a38e62f217f0bad92ae7d7ec0e
JDAC20 <157185109+JDAC20@users.noreply.github.com>
2024-01-25 21:18:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function login() {
   var password = document.getElementById("password").value;
 
   // Expresión regular para verificar un formato de correo electrónico
-  var emailRegex = /^[a-zA-Z0-9._%+-]+@;
+  var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 
   // Expresión regular para verificar que la contraseña tenga al menos 6 caracteres
-  var passwordRegex = /.{6,}/;
+  var passwordRegex = /.{8,}/;
 
   if (!emailRegex.test(username)) {
     alert("Ingresa un correo electrónico válido");
