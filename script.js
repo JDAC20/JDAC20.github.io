b7c792ed44085df283623a6740c27f2430d1b20c
JDAC20 <157185109+JDAC20@users.noreply.github.com>
2024-01-25 21:12:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function login() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
+  // Expresión regular para verificar un formato de correo electrónico
+  var emailRegex = /^[a-zA-Z0-9._%+-]+@;
+
+  // Expresión regular para verificar que la contraseña tenga al menos 6 caracteres
+  var passwordRegex = /.{6,}/;
+
+  if (!emailRegex.test(username)) {
+    alert("Ingresa un correo electrónico válido");
+  } else if (!passwordRegex.test(password)) {
+    alert("La contraseña debe tener al menos 8 caracteres");
+  } else {
+    alert("Login exitoso");
+  }
+}
